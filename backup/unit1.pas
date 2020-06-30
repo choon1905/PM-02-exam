967c98f6ef300ccdc697c661d42f4b0674ed7c8c
Агабабян Арсен <agababyan.arsen@bk.ru>
2020-06-30 13:09:33 +0500

    Проект в процессе разработки

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -5,10 +5,23 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Buttons, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
   private
 
   public
